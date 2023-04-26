ad0a9fbeb37b17b97eb834ec2d48ae9a066e9a36
sheakabhi25 <ag893573@gmail.com>
2023-04-27 01:43:21 +0530

    fatching by news api id

--- src/NewsCom.jsx
+++ src/NewsCom.jsx
@@ -14,10 +14,10 @@ export default function NewsCom(props) {
       </h3> */}
       <div className="card  my-3 " style={{ width: "18rem", ...newsstyle }}    >
 
-          <h3  style={{fontSize:"20px "}}>{props.tittle}...</h3>
-        <img src={props.url} className="card-img-top" alt="..." />
+          <h5  style={{fontSize:"20px "}}>{props.tittle}</h5>
+        <img src={!props.url ? "https://storage.googleapis.com/afs-prod/media/a3a78896c9344d898a4c67537781e499/3000.jpeg" : props.url} className="card-img-top" alt="..." />
         <div className="card-body  my-5 ">
-          <p className="card-text">{props.desc}... </p>
+          <p className="card-text">{props.desc} </p>
 <a href={props.newsurl}  target="_blank" rel="noreferrer" className='btn btn-success'> Read more</a>
 
         </div>
